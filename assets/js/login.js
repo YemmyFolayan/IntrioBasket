a246ff9b5b223ad4099917ce16f8efdab2205389
YemmyFolayan <foyemc@gmail.com>
2020-08-23 01:33:09 +0100

    integrating login API

---
+++ assets/js/login.js
@@ -0,0 +1,39 @@
+const activateLoginApicall = () => {
+  const formElement = document.getElementById("form");
+  // 'email' is the value of the name attribute of the input element for email
+  // 'password' is the value of the name attribute of the input element for password
+  const email = formElement.elements["email"].value;
+  const password = formElement.elements["password"].value;
+  const url = "http://intriobasket.pexceptos.com/api/user/login";
+
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: "POST",
+      body: {
+        email,
+        password,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          reject(response);
+        }
+      })
+      .then((data) => resolve(data))
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+const login = async (event) => {
+  event.preventDefault();
+  try {
+    const data = await activateLoginApicall();
+    //whatever you want to do with data
+  } catch (error) {
+    //oops error, whatever you want to do with the error
+  }
+};
