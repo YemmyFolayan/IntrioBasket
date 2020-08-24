05a891db15fc8bb40d1bc38d4e833144f91d31a8
YemmyFolayan <foyemc@gmail.com>
2020-08-24 12:41:27 +0100

    integrating user registration API

--- assets/js/foodlist.js
+++ assets/js/foodlist.js
@@ -3,6 +3,8 @@ const app = document.getElementById("root");
 const containers = document.createElement("div");
 containers.setAttribute("class", "containers");
 
+
+
 app.appendChild(containers);
 
 var request = new XMLHttpRequest();
@@ -11,6 +13,8 @@ request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
+    //movie is self define parameter to loop through each of the json data
+    // to get each properties
     data.forEach((movie) => {
       const card = document.createElement("div");
       card.setAttribute("class", "card");
@@ -19,7 +23,7 @@ request.onload = function () {
       h6.textContent = movie.title;
 
       const p = document.createElement("p");
-      p.class = 'fooddetails';
+      p.class = "fooddetails";
       // p.id = 'food';
       movie.description = movie.description.substring(0, 300);
       p.textContent = `${movie.description}...`;
@@ -36,3 +40,61 @@ request.onload = function () {
 };
 
 request.send();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = document.getElementById("root");
+
+// const containers = document.createElement("div");
+// containers.setAttribute("class", "containers");
+
+// app.appendChild(containers);
+
+// var request = new XMLHttpRequest();
+// request.open("GET", "https://ghibliapi.herokuapp.com/films", true);
+// request.onload = function () {
+//   // Begin accessing JSON data here
+//   var data = JSON.parse(this.response);
+//   if (request.status >= 200 && request.status < 400) {
+//     //movie is self define parameter to loop through each of the json data
+//     // to get each properties
+//     data.forEach((movie) => {
+//       const card = document.createElement("div");
+//       card.setAttribute("class", "card");
+
+//       const h6 = document.createElement("h6");
+//       h6.textContent = movie.title;
+
+//       const p = document.createElement("p");
+//       p.class = "fooddetails";
+//       // p.id = 'food';
+//       movie.description = movie.description.substring(0, 300);
+//       p.textContent = `${movie.description}...`;
+
+//       containers.appendChild(card);
+//       card.appendChild(h6);
+//       card.appendChild(p);
+//     });
+//   } else {
+//     const errorMessage = document.createElement("marquee");
+//     errorMessage.textContent = `Gah, it's not working!`;
+//     app.appendChild(errorMessage);
+//   }
+// };
+
+// request.send();
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,39 +1,113 @@
-const activateLoginApicall = () => {
-  const formElement = document.getElementById("form");
-  // 'email' is the value of the name attribute of the input element for email
-  // 'password' is the value of the name attribute of the input element for password
-  const email = formElement.elements["email"].value;
-  const password = formElement.elements["password"].value;
-  const url = "http://intriobasket.pexceptos.com/api/user/login";
+window.addEventListener("load", function () {
+  const activateLoginApicall = () => {
+    const formElement = document.getElementById("form");
+    // 'email' is the value of the name attribute of the input element for email
+    // 'password' is the value of the name attribute of the input element for password
+    const email = formElement.elements["email"].value;
+    const password = formElement.elements["password"].value;
+    const url = "http://intriobasket.pexceptos.com/api/user/login";
 
-  return new Promise((resolve, reject) => {
-    fetch(url, {
-      method: "POST",
-      body: {
-        email,
-        password,
-      },
-    })
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          reject(response);
-        }
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "post",
+        body: {
+          email,
+          password,
+        },
       })
-      .then((data) => resolve(data))
-      .catch((err) => {
-        reject(err);
-      });
-  });
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            reject(response);
+          }
+        })
+        .then((data) => resolve(data))
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  };
 };
+  const login = async (event) => {
+    event.preventDefault();
+    try {
+      const data = await activateLoginApicall();
+      alert("opppsssss";)
+      //whatever you want to do with data
+    } catch (error) {
+     
+      //oops error, whatever you want to do with the error
+    }
+  };
 
-const login = async (event) => {
-  event.preventDefault();
-  try {
-    const data = await activateLoginApicall();
-    //whatever you want to do with data
-  } catch (error) {
-    //oops error, whatever you want to do with the error
-  }
-};
+
+// <form id="myForm">
+//   <label for="myName">Send me your name:</label>
+//   <input id="myName" name="name" value="John">
+//   <input type="submit" value="Send Me!">
+// </form>
+
+// window.addEventListener( "load", function () {
+//   function sendData() {
+//     const XHR = new XMLHttpRequest();
+
+//     // Bind the FormData object and the form element
+//     const FD = new FormData( form );
+
+//     // Define what happens on successful data submission
+//     XHR.addEventListener( "load", function(event) {
+//       alert( event.target.responseText );
+//     } );
+
+//     // Define what happens in case of error
+//     XHR.addEventListener( "error", function( event ) {
+//       alert( 'Oops! Something went wrong.' );
+//     } );
+
+//     // Set up our request
+//     XHR.open( "POST", "https://example.com/cors.php" );
+
+//     // The data sent is what the user provided in the form
+//     XHR.send( FD );
+//   }
+
+//   // Access the form element...
+//   const form = document.getElementById( "myForm" );
+
+//   // ...and take over its submit event.
+//   form.addEventListener( "submit", function ( event ) {
+//     event.preventDefault();
+
+//     sendData();
+//   } );
+// } );
+
+// /////////////////////
+
+// <form  id="postData">
+//     <div>
+//         <input type="text" name="" id="tittle">
+//     </div>
+//     <div>
+//         <textarea name="" id="body" cols="20" rows="5"></textarea>
+//     </div>
+//     <input type="submit" value="SEND POST">
+// </form>
+
+// document.getElementById('postData').addEventListener('submit', postData);
+
+// function postData(event){
+//             event.preventDefault();
+
+//             let tittle = document.getElementById('tittle').value;
+//             let body = document.getElementById('body').value;
+
+//             fetch('https://jsonplaceholder.typicode.com/posts', {
+//                 method: 'POST',
+//                 headers : new Headers(),
+//                 body:JSON.stringify({tittle:tittle, body:body})
+//             }).then((res) => res.json())
+//             .then((data) =>  console.log(data))
+//             .catch((err)=>console.log(err))
+//         }
