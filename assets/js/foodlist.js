99f5f1f5bcb0cb165ed6b70db25692964df322de
YemmyFolayan <foyemc@gmail.com>
2020-08-25 14:21:22 +0100

    integrating user registration API

--- assets/js/foodlist.js
+++ assets/js/foodlist.js
@@ -20,7 +20,7 @@ request.onload = function () {
       h6.textContent = food_product_name;
 
       const p = document.createElement("p");
-      p.textContent = cost;
+      p.textContent = `#${cost}`;
 
       const img = document.createElement("img");
       img.setAttribute("class", "product-img");
@@ -28,12 +28,37 @@ request.onload = function () {
       // image_link = image_link.substring(0, 300);
       img.src = `${image_link}...`;
 
+      const carty = document.createElement("div");
+      carty.setAttribute("class", "product-select");
+
+      const addcart = document.createElement("button");
+      addcart.setAttribute("class", "add-to-cart");
+
+      const addlist = document.createElement("button");
+      addlist.setAttribute("class", "add-to-wishlist");
+
+      const addcomp = document.createElement("button");
+      addcomp.setAttribute("class", "add-to-compare");
+
+      const addview = document.createElement("button");
+      addview.setAttribute("class", "quickview");
+
+      // <button class="add-to-wishlist round-icon-btn"> <i class="icon_heart_alt"></i></button>
+      // <button class="add-to-cart round-icon-btn">  <i class="icon_bag_alt"></i></button>
+      // <button class="add-to-compare round-icon-btn"><i class="fas fa-random"></i></button>
+      // <button class="quickview round-icon-btn"> <i class="far fa-eye"></i></button>
+
       console.log(image_link);
 
       containers.appendChild(card);
       card.appendChild(img);
       card.appendChild(h6);
       card.appendChild(p);
+      card.appendChild(carty);
+      carty.appendChild(addcart);
+      carty.appendChild(addlist);
+      carty.appendChild(addcomp);
+      carty.appendChild(addview);
     });
   } else {
     const errorMessage = document.createElement("marquee");
--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,54 +1,51 @@
-console.log("hi");
+// const activateLoginApicall = () => {
+const formElement = document.getElementById("form");
 
-var x = document.getElementById("form").elements.namedItem("email").value;
 
-const activateLoginApicall = () => {
-  const formElement = document.getElementById("form");
-  console.log(formElement);
-// 'email' is the value of the name attribute of the input element for email
-// 'password' is the value of the name attribute of the input element for password
-  const email = formElement.elements["email"].value;
-  const password = formElement.elements["password"].value;
-
+  // 'email' is the value of the name attribute of the input element for email
+  // 'password' is the value of the name attribute of the input element for password
+const email = formElement.elements["email"].value;
+const password = formElement.elements["password"].value;
 console.log(email);
-
+console.log(formElement);
 const url = "http://intriobasket.pexceptos.com/api/user/create";
 
-return new Promise((resolve, reject) => {
-  fetch(url, {
-    method: "POST",
-    body: {
-      email,
-      password,
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        reject(response);
-      }
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: "POST",
+      body: {
+        email,
+        password,
+      },
     })
-    .then((data) => resolve(data))
-    .catch((err) => {
-      reject(err);
-    });
-});
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          reject(response);
+        }
+      })
+      .then((data) => resolve(data))
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
 
-// // const login = async (event) => {
-// //   event.preventDefault();
-// //   try {
-// //     const data = await activateLoginApicall();
+const login = async (event) => {
+  event.preventDefault();
+  try {
+    const data = await activateLoginApicall();
 
-// //     //whatever you want to do with data
-// //   } catch (error) {
-// //     //oops error, whatever you want to do with the error
-// //   }
-// // };
+    //whatever you want to do with data
+  } catch (error) {
+    //oops error, whatever you want to do with the error
+  }
+};
 
-// // const fetchHelper = ("http://intriobasket.pexceptos.com/api/user/create", config = {}) => {
-// //      const token = localStorage.getItem("token")
-// //      return fetch(
-// //      "http://intriobasket.pexceptos.com/api/user/create",
-// //      { ...config, headers: { ...config.headers, Authorization: `Bearer ${token}` } })
-// // }
+// const fetchHelper = ("http://intriobasket.pexceptos.com/api/user/create", config = {}) => {
+//      const token = localStorage.getItem("token")
+//      return fetch(
+//      "http://intriobasket.pexceptos.com/api/user/create",
+//      { ...config, headers: { ...config.headers, Authorization: `Bearer ${token}` } })
+// }
