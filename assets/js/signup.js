8e7b146a9f95edffda19afd1dcf8550c83fe3166
YemmyFolayan <foyemc@gmail.com>
2020-08-27 21:37:01 +0100

    integrating user registration API

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,77 +1,74 @@
-// const activateLoginApicall = () => {
-const formElement = document.getElementById("form");
+// // const activateLoginApicall = () => {
+// const formElement = document.getElementById("form");
 
-// 'email' is the value of the name attribute of the input element for email
-// 'password' is the value of the name attribute of the input element for password
-//  const email = formElement.elements["email"].value;
-//  const password = formElement.elements["password"].value;
-//  console.log(email);
-//  console.log(formElement);
-const url = "http://intriobasket.pexceptos.com/api/user/create";
+// // 'email' is the value of the name attribute of the input element for email
+// // 'password' is the value of the name attribute of the input element for password
+// //  const email = formElement.elements["email"].value;
+// //  const password = formElement.elements["password"].value;
+// //  console.log(email);
+// //  console.log(formElement);
+// const url = "http://intriobasket.pexceptos.com/api/user/create";
 
-formElement
-    .addEventListener("submit", function (e) {
-        e.preventDefault();
+// formElement
+//   .addEventListener("submit", function (e) {
+//     e.preventDefault();
 
-        const formData = new FormData(this);
-        const searchParams = new URLSearchParams();
+//     const formData = new FormData(this);
+//       const searchParams = new URLSearchParams();
+      
+//     return new Promise((resolve, reject) => {
 
-        return new Promise((resolve, reject) => {
+//     fetch(url, {
+//       method: "post",
+//       body: searchParams,
+//     }).then((response) => {
+//       if (response.ok) {
+//         return response.json();
+//       } else {
+//         reject(response);
+//       }
+//     });
+//   })
+//   .then((data) => resolve(data))
+//   .catch((err) => {
+//     reject(err);
+//   });
 
-            fetch(url, {
-                method: "post",
-                body: searchParams,
-            }).then((response) => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    reject(response);
-                }
-            });
-        })
-            .then((data) => resolve(data))
-            .catch((err) => {
-                reject(err);
-            });
-        
-    }
-}
+// //  console.log(formData);
+// //    return new Promise((resolve, reject) => {
+// //      fetch(url, {
+// //        method: "POST",
+// //        body: search,
+// //      })
+// //        .then((response) => {
+// //          if (response.ok) {
+// //            return response.json();
+// //          } else {
+// //            reject(response);
+// //          }
+// //        })
+// //        .then((data) => resolve(data))
+// //        log
+// //        .catch((err) => {
+// //          reject(err);
+// //        });
+// //    });
+// //  };
 
-//  console.log(formData);
-//    return new Promise((resolve, reject) => {
-//      fetch(url, {
-//        method: "POST",
-//        body: search,
-//      })
-//        .then((response) => {
-//          if (response.ok) {
-//            return response.json();
-//          } else {
-//            reject(response);
-//          }
-//        })
-//        .then((data) => resolve(data))
-//        log
-//        .catch((err) => {
-//          reject(err);
-//        });
-//    });
-//  };
+// //  const login = async (event) => {
+// //    event.preventDefault();
+// //    try {
+// //      const data = await activateLoginApicall();
 
-//  const login = async (event) => {
-//    event.preventDefault();
-//    try {
-//      const data = await activateLoginApicall();
+// //      //whatever you want to do with data
+// //    } catch (error) {
+// //      //oops error, whatever you want to do with the error
+// //    }
+// //  };
 
-//      //whatever you want to do with data
-//    } catch (error) {
-//      //oops error, whatever you want to do with the error
-//    }
-//  };
-
-// const fetchHelper = ("http://intriobasket.pexceptos.com/api/user/create", config = {}) => {
-//      const token = localStorage.getItem("token")
-//      return fetch(
-//      "http://intriobasket.pexceptos.com/api/user/create",
-//      { ...config, headers: { ...config.headers, Authorization: `Bearer ${token}` } })
-// }
+ const fetchHelper = ("http://intriobasket.pexceptos.com/api/user/create", config = {}) => {
+      const token = localStorage.getItem("token")
+      return fetch(
+      "http://intriobasket.pexceptos.com/api/user/create",
+      { ...config, headers: { ...config.headers, Authorization: `Bearer ${token}` } })
+ }
