d7ccb37a3d6ada1717303bae2abe693b8c5cc401
YemmyFolayan <foyemc@gmail.com>
2020-08-29 23:31:40 +0100

    integrating user registration API

--- assets/js/foodlist.js
+++ assets/js/foodlist.js
@@ -5,22 +5,10 @@ containers.setAttribute("class", "containers");
 
 app.appendChild(containers);
 
-
-
-
-// var request = new XMLHttpRequest();
-// request.open("GET", "http://intriobasket.pexceptos.com/api/food", true);
-// request.onload = function () {
-  
-  
-  const url = "http://intriobasket.pexceptos.com/api/user/create";
-  
-  return new Promise((resolve, reject) => {
-    fetch(url, {
-      method: "GET",
-      
-    },
-  })
+const url = "http://intriobasket.pexceptos.com/api/food";
+fetch(url, {
+  method: "GET",
+})
   .then((response) => {
     if (response.ok) {
       return response.json();
@@ -28,155 +16,62 @@ app.appendChild(containers);
       reject(response);
     }
   })
-  .then((data) => resolve(data))
-  .catch((err) => {
-    reject(err);
-  });
-  
-  //   // Begin accessing JSON data here
-  
-const login = async (event) => {
-  event.preventDefault();
-  try {
-    
+  .then(function (data) {
     if (request.status >= 200 && request.status < 400) {
-      const data = await activateLoginApicall();
-      //whatever you want to do with data
-  // var data = JSON.parse(this.response);
-  console.log(data);
-    data.payload.forEach(({ food_product_name, image_link, cost }) => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
+      data.payload.forEach(({ food_product_name, image_link, cost }) => {
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
 
-      const h6 = document.createElement("h6");
-      h6.textContent = food_product_name;
+        const h6 = document.createElement("h6");
+        h6.textContent = food_product_name;
 
-      const p = document.createElement("p");
-      p.textContent = `#${cost}`;
-      console.log(cost);
+        const p = document.createElement("p");
+        p.textContent = `#${cost}`;
+        console.log(cost);
 
-      const img = document.createElement("img");
-      img.setAttribute("class", "product-img");
+        const img = document.createElement("img");
+        img.setAttribute("class", "product-img");
 
-      // image_link = image_link.substring(0, 300);
-      img.src = `${image_link}...`;
+        // image_link = image_link.substring(0, 300);
+        img.src = `${image_link}...`;
 
-      const carty = document.createElement("div");
-      carty.setAttribute("class", "product-select");
+        const carty = document.createElement("div");
+        carty.setAttribute("class", "product-select");
 
-      const addcart = document.createElement("button");
-      addcart.setAttribute("class", "icon_bag_alt");
+        const addcart = document.createElement("button");
+        addcart.setAttribute("class", "icon_bag_alt");
 
-      const addlist = document.createElement("button");
-      addlist.setAttribute("class", "icon_heart_alt");
+        const addlist = document.createElement("button");
+        addlist.setAttribute("class", "icon_heart_alt");
 
-      const addcomp = document.createElement("button");
-      addcomp.setAttribute("class", "fas fa-random");
+        const addcomp = document.createElement("button");
+        addcomp.setAttribute("class", "fas fa-random");
 
-      const addview = document.createElement("button");
-      addview.setAttribute("class", "far fa-eye");
+        const addview = document.createElement("button");
+        addview.setAttribute("class", "far fa-eye");
 
-      // <button class="add-to-wishlist round-icon-btn"> <i class="icon_heart_alt"></i></button>
-      // <button class="add-to-cart round-icon-btn">  <i class="icon_bag_alt"></i></button>
-      // <button class="add-to-compare round-icon-btn"><i class="fas fa-random"></i></button>
-      // <button class="quickview round-icon-btn"> <i class="far fa-eye"></i></button>
+        // <button class="add-to-wishlist round-icon-btn"> <i class="icon_heart_alt"></i></button>
+        // <button class="add-to-cart round-icon-btn">  <i class="icon_bag_alt"></i></button>
+        // <button class="add-to-compare round-icon-btn"><i class="fas fa-random"></i></button>
+        // <button class="quickview round-icon-btn"> <i class="far fa-eye"></i></button>
 
-      console.log(image_link);
+        console.log(image_link);
 
-      containers.appendChild(card);
-      card.appendChild(img);
-      card.appendChild(h6);
-      card.appendChild(p);
-      card.appendChild(carty);
-      carty.appendChild(addcart);
-      carty.appendChild(addlist);
-      carty.appendChild(addcomp);
-      carty.appendChild(addview);
-    });
-  } else {
-    const errorMessage = document.createElement("marquee");
-    errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
-  }
-};
+        containers.appendChild(card);
+        card.appendChild(img);
+        card.appendChild(h6);
+        card.appendChild(p);
+        card.appendChild(carty);
+        carty.appendChild(addcart);
+        carty.appendChild(addlist);
+        carty.appendChild(addcomp);
+        carty.appendChild(addview);
+      });
+    } else {
+      const errorMessage = document.createElement("marquee");
+      errorMessage.textContent = `Gah, it's not working!`;
+      app.appendChild(errorMessage);
+    }
+  });
 
-request.send();
-
-// Create element and append the value to it
-
-
-
-
-// const app = document.getElementById("root");
-
-// const containers = document.createElement("div");
-// containers.setAttribute("class", "containers");
-
-// app.appendChild(containers);
-
-// var request = new XMLHttpRequest();
-// request.open("GET", "http://intriobasket.pexceptos.com/api/food", true);
-// request.onload = function () {
 //   // Begin accessing JSON data here
-//   var data = JSON.parse(this.response);
-//   console.log(data);
-//   if (request.status >= 200 && request.status < 400) {
-//     data.payload.forEach(({ food_product_name, image_link, cost }) => {
-//       const card = document.createElement("div");
-//       card.setAttribute("class", "card");
-
-//       const h6 = document.createElement("h6");
-//       h6.textContent = food_product_name;
-
-//       const p = document.createElement("p");
-//       p.textContent = `#${cost}`;
-//       console.log(cost);
-
-//       const img = document.createElement("img");
-//       img.setAttribute("class", "product-img");
-
-//       // image_link = image_link.substring(0, 300);
-//       img.src = `${image_link}...`;
-
-//       const carty = document.createElement("div");
-//       carty.setAttribute("class", "product-select");
-
-//       const addcart = document.createElement("button");
-//       addcart.setAttribute("class", "icon_bag_alt");
-
-//       const addlist = document.createElement("button");
-//       addlist.setAttribute("class", "icon_heart_alt");
-
-//       const addcomp = document.createElement("button");
-//       addcomp.setAttribute("class", "fas fa-random");
-
-//       const addview = document.createElement("button");
-//       addview.setAttribute("class", "far fa-eye");
-
-//       // <button class="add-to-wishlist round-icon-btn"> <i class="icon_heart_alt"></i></button>
-//       // <button class="add-to-cart round-icon-btn">  <i class="icon_bag_alt"></i></button>
-//       // <button class="add-to-compare round-icon-btn"><i class="fas fa-random"></i></button>
-//       // <button class="quickview round-icon-btn"> <i class="far fa-eye"></i></button>
-
-//       console.log(image_link);
-
-//       containers.appendChild(card);
-//       card.appendChild(img);
-//       card.appendChild(h6);
-//       card.appendChild(p);
-//       card.appendChild(carty);
-//       carty.appendChild(addcart);
-//       carty.appendChild(addlist);
-//       carty.appendChild(addcomp);
-//       carty.appendChild(addview);
-//     });
-//   } else {
-//     const errorMessage = document.createElement("marquee");
-//     errorMessage.textContent = `Gah, it's not working!`;
-//     app.appendChild(errorMessage);
-//   }
-// };
-
-// request.send();
-
-// // Create element and append the value to it
