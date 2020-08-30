1ffb267f6872ecbf78d614c9c10ab426c4f977ba
YemmyFolayan <foyemc@gmail.com>
2020-08-30 02:13:20 +0100

    integrating user registration API

--- assets/js/foodlist.js
+++ assets/js/foodlist.js
@@ -5,7 +5,8 @@ containers.setAttribute("class", "containers");
 
 app.appendChild(containers);
 
-const url ="https://cors-anywhere.herokuapp.com/http://intriobasket.pexceptos.com/api/food";
+const url =
+  "https://cors-anywhere.herokuapp.com/http://intriobasket.pexceptos.com/api/food";
 console.log("foodlists");
 
 fetch(url)
@@ -17,63 +18,58 @@ fetch(url)
     }
   })
   .then((data) => {
-    if (request.status >= 200 && request.status < 400) {
-      data.payload.forEach(({ food_product_name, image_link, cost }) => {
-        const card = document.createElement("div");
-        card.setAttribute("class", "card");
+    // if (request.status >= 200 && request.status < 400) {
+    data.payload.forEach(({ food_product_name, image_link, cost }) => {
+      const card = document.createElement("div");
+      card.setAttribute("class", "card");
 
-        const h6 = document.createElement("h6");
-        h6.textContent = food_product_name;
+      const h6 = document.createElement("h6");
+      h6.textContent = food_product_name;
 
-        const p = document.createElement("p");
-        p.textContent = `#${cost}`;
+      const p = document.createElement("p");
+      p.textContent = `#${cost}`;
 
-        console.log(cost);
+      console.log(cost);
 
-        const img = document.createElement("img");
-        img.setAttribute("class", "product-img");
+      const img = document.createElement("img");
+      img.setAttribute("class", "product-img");
 
-        // image_link = image_link.substring(0, 300);
-        img.src = `${image_link}...`;
+      // image_link = image_link.substring(0, 300);
+      img.src = `${image_link}...`;
 
-        const carty = document.createElement("div");
-        carty.setAttribute("class", "product-select");
+      const carty = document.createElement("div");
+      carty.setAttribute("class", "product-select");
 
-        const addcart = document.createElement("button");
-        addcart.setAttribute("class", "icon_bag_alt");
+      const addcart = document.createElement("button");
+      addcart.setAttribute("class", "icon_bag_alt");
 
-        const addlist = document.createElement("button");
-        addlist.setAttribute("class", "icon_heart_alt");
+      const addlist = document.createElement("button");
+      addlist.setAttribute("class", "icon_heart_alt");
 
-        const addcomp = document.createElement("button");
-        addcomp.setAttribute("class", "fas fa-random");
+      const addcomp = document.createElement("button");
+      addcomp.setAttribute("class", "fas fa-random");
 
-        const addview = document.createElement("button");
-        addview.setAttribute("class", "far fa-eye");
+      const addview = document.createElement("button");
+      addview.setAttribute("class", "far fa-eye");
 
-        // <button class="add-to-wishlist round-icon-btn"> <i class="icon_heart_alt"></i></button>
-        // <button class="add-to-cart round-icon-btn">  <i class="icon_bag_alt"></i></button>
-        // <button class="add-to-compare round-icon-btn"><i class="fas fa-random"></i></button>
-        // <button class="quickview round-icon-btn"> <i class="far fa-eye"></i></button>
+      // <button class="add-to-wishlist round-icon-btn"> <i class="icon_heart_alt"></i></button>
+      // <button class="add-to-cart round-icon-btn">  <i class="icon_bag_alt"></i></button>
+      // <button class="add-to-compare round-icon-btn"><i class="fas fa-random"></i></button>
+      // <button class="quickview round-icon-btn"> <i class="far fa-eye"></i></button>
 
-        console.log(image_link);
-        console.log("hi");
+      console.log(image_link);
+      console.log("hi");
 
-        containers.appendChild(card);
-        card.appendChild(img);
-        card.appendChild(h6);
-        card.appendChild(p);
-        card.appendChild(carty);
-        carty.appendChild(addcart);
-        carty.appendChild(addlist);
-        carty.appendChild(addcomp);
-        carty.appendChild(addview);
-      });
-    } else {
-      const errorMessage = document.createElement("marquee");
-      errorMessage.textContent = `Gah, it's not working!`;
-      app.appendChild(errorMessage);
-    }
+      containers.appendChild(card);
+      card.appendChild(img);
+      card.appendChild(h6);
+      card.appendChild(p);
+      card.appendChild(carty);
+      carty.appendChild(addcart);
+      carty.appendChild(addlist);
+      carty.appendChild(addcomp);
+      carty.appendChild(addview);
+    });
   });
 
 //   // Begin accessing JSON data here
