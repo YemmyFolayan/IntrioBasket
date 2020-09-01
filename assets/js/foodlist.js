dd65b969d46daf64f08b535f70dda778ddbd24b3
YemmyFolayan <foyemc@gmail.com>
2020-09-01 13:43:57 +0100

    integrating user registration API

--- assets/js/foodlist.js
+++ assets/js/foodlist.js
@@ -10,10 +10,11 @@ container.appendChild(subcontainer);
 
 const url =
   "https://cors-anywhere.herokuapp.com/http://intriobasket.pexceptos.com/api/food";
-console.log("foodlistssssssssssssssss");
+console.log("foodlists");
 
 const cachedFetch = (url, options) => {
-  let expiry = 1 * 60; // 1 min default
+  console.log("casched fetch");
+  let expiry = 5 * 60; // 5 min default
   if (typeof options === "number") {
     expiry = options;
     options = undefined;
@@ -144,10 +145,115 @@ const cachedFetch = (url, options) => {
               addview.appendChild(iconview);
             });
           });
+
+        // Default 5 min
+        cachedFetch("https://httpbin.org/get")
+          .then((r) => r.json())
+          .then((info) => {
+            console.log("Your origin is " + info.origin);
+          });
+
+        // 2 minutes
+        cachedFetch("http://httpbin.org/gzip", 2 * 60)
+          .then((r) => r.json())
+          .then((info) => {
+            console.log("Your origin (with gzip) is " + info.origin);
+          });
+
+        // 1 minute
+        cachedFetch("http://httpbin.org/ip", { seconds: 60 })
+          .then((r) => r.json())
+          .then((info) => {
+            console.log("Your origin (by ip only) is " + info.origin);
+          });
       }
     }
   });
 };
+
+// // const cachedFetch = (url, options) => {
+// //   let expiry = 5 * 60 // 5 min default
+// //   if (typeof options === 'number') {
+// //     expiry = options
+// //     options = undefined
+// //   } else if (typeof options === 'object') {
+// //     // I hope you didn't set it to 0 seconds
+// //     expiry = options.seconds || expiry
+// //   }
+// //   // Use the URL as the cache key to sessionStorage
+// //   let cacheKey = url
+// //   let cached = localStorage.getItem(cacheKey)
+// //   let whenCached = localStorage.getItem(cacheKey + ':ts')
+// //   if (cached !== null && whenCached !== null) {
+// //     // it was in sessionStorage! Yay!
+// //     // Even though 'whenCached' is a string, this operation
+// //     // works because the minus sign tries to convert the
+// //     // string to an integer and it will work.
+// //     let age = (Date.now() - whenCached) / 1000
+// //     if (age < expiry) {
+// //       let response = new Response(new Blob([cached]))
+// //       return Promise.resolve(response)
+// //     } else {
+// //       // We need to clean up this old key
+// //       localStorage.removeItem(cacheKey)
+// //       localStorage.removeItem(cacheKey + ':ts')
+// //     }
+// //   }
+
+// //   return fetch(url, options).then(response => {
+// //     // let's only store in cache if the content-type is
+// //     // JSON or something non-binary
+// //     if (response.status === 200) {
+// //       let ct = response.headers.get('Content-Type')
+// //       if (ct && (ct.match(/application\/json/i) || ct.match(/text\//i))) {
+// //         // There is a .json() instead of .text() but
+// //         // we're going to store it in sessionStorage as
+// //         // string anyway.
+// //         // If we don't clone the response, it will be
+// //         // consumed by the time it's returned. This
+// //         // way we're being un-intrusive.
+// //         response.clone().text().then(content => {
+// //           localStorage.setItem(cacheKey, content)
+// //           localStorage.setItem(cacheKey + ':ts', Date.now())
+// //         })
+// //       }
+// //     }
+// //     return response
+// //   })
+// // }
+
+// // // Default 5 min
+// // cachedFetch('https://httpbin.org/get')
+// //   .then(r => r.json())
+// //   .then(info => {
+// //     console.log('Your origin is ' + info.origin)
+// //   })
+
+// // // 2 minutes
+// // cachedFetch('http://httpbin.org/gzip', 2 * 60)
+// //   .then(r => r.json())
+// //   .then(info => {
+// //     console.log('Your origin (with gzip) is ' + info.origin)
+// //   })
+
+// // // 1 minute
+// // cachedFetch('http://httpbin.org/ip', { seconds: 60 })
+// //   .then(r => r.json())
+// //   .then(info => {
+// //     console.log('Your origin (by ip only) is ' + info.origin)
+// //   })
+
+// cachedFetch('https://httpbin.org/html')
+// .then(r => r.text())
+// .then(document => {
+//   console.log('Document has ' + document.match(/<p>/).length + ' paragraphs')
+// })
+
+// cachedFetch('https://httpbin.org/image/png')
+// .then(r => r.blob())
+// .then(image => {
+//   console.log('Image is ' + image.size + ' bytes')
+// })
 
 // // // const app = document.getElementById("root");
 
