fa7364d102d7ada0821520beac51798aa2484296
YemmyFolayan <foyemc@gmail.com>
2020-09-02 15:54:39 +0100

    integrating user registration API

--- assets/js/foodlist.js
+++ assets/js/foodlist.js
@@ -1,13 +1,13 @@
 const app = document.getElementById("root");
 
 const container = document.createElement("div");
-container.setAttribute("class", "row no-gutters-sm");
+container.setAttribute("class", "product");
 
-const subcontainer = document.createElement("div");
-subcontainer.setAttribute("class", "displayflex");
+// const subcontainer = document.createElement("div");
+// subcontainer.setAttribute("class", "displayflex");
 
 app.appendChild(container);
-container.appendChild(subcontainer);
+// container.appendChild(subcontainer);
 
 const url =
   "https://cors-anywhere.herokuapp.com/http://intriobasket.pexceptos.com/api/food";
@@ -36,12 +36,17 @@ fetch(url)
     console.log(top10);
     // if (request.status >= 200 && request.status < 400) {
     top10.forEach(({ food_product_name, image_link, cost }) => {
-      const product = document.createElement("div");
-      product.setAttribute("class", "product");
+      const productdetails = document.createElement("a");
+      productdetails.setAttribute("class", "product-img");
+      productdetails.setAttribute("href", "shop_detail.html");
+
+
+
 
       const img = document.createElement("img");
-      img.setAttribute("class", "product-img");
+      img.setAttribute("alt", "product");
       img.src = `${image_link}...`;
+
 
       const h5 = document.createElement("h5");
       h5.textContent = food_product_name;
@@ -58,6 +63,8 @@ fetch(url)
 
       h3.setAttribute("class", "product-price");
 
+
+     
       const productselect = document.createElement("div");
       productselect.setAttribute("class", "product-select");
 
@@ -85,16 +92,17 @@ fetch(url)
       const iconview = document.createElement("i");
       iconview.setAttribute("class", "far fa-eye");
 
-      subcontainer.appendChild(product);
 
-      product.appendChild(img);
-      product.appendChild(h5);
+      container.appendChild(productdetails);
 
-      product.appendChild(h3cost);
+      productdetails.appendChild(img);
+      container.appendChild(h5);
 
-      product.appendChild(h3);
+      container.appendChild(h3cost);
 
-      product.appendChild(productselect);
+      container.appendChild(h3);
+
+      container.appendChild(productselect);
 
       productselect.appendChild(addlist);
 
@@ -640,3 +648,121 @@ fetch(url)
 // // // //      }
 // // // //      return response
 // // // //    })
+
+//LATEST
+
+// const app = document.getElementById("root");
+
+// const container = document.createElement("div");
+// container.setAttribute("class", "row no-gutters-sm");
+
+// const subcontainer = document.createElement("div");
+// subcontainer.setAttribute("class", "displayflex");
+
+// app.appendChild(container);
+// container.appendChild(subcontainer);
+
+// const url =
+//   "https://cors-anywhere.herokuapp.com/http://intriobasket.pexceptos.com/api/food";
+// console.log("foodlists");
+
+// fetch(url)
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       reject(response);
+//     }
+//   })
+//   .then((data) => {
+//     // First, parse the JSON into an array of Objects:
+
+//     // var data = JSON.parse(json);
+
+//     // Then combine sort and slice to achieve your goal:
+//     var top10 = data.payload
+//       .sort(function (a, b) {
+//         return a.Variable1 < b.Variable1 ? 1 : -1;
+//       })
+//       .slice(0, 2);
+
+//     console.log(top10);
+//     // if (request.status >= 200 && request.status < 400) {
+//     top10.forEach(({ food_product_name, image_link, cost }) => {
+//       const product = document.createElement("div");
+//       product.setAttribute("class", "product");
+
+//       const img = document.createElement("img");
+//       img.setAttribute("class", "product-img");
+//       img.src = `${image_link}...`;
+
+//       const h5 = document.createElement("h5");
+//       h5.textContent = food_product_name;
+
+//       h5.setAttribute("class", "product-type");
+
+//       const h3cost = document.createElement("h3");
+//       h3cost.textContent = food_product_name;
+
+//       h3cost.setAttribute("class", "product-name");
+
+//       const h3 = document.createElement("h3");
+//       h3.textContent = `NGN ${cost}`;
+
+//       h3.setAttribute("class", "product-price");
+
+//       const productselect = document.createElement("div");
+//       productselect.setAttribute("class", "product-select");
+
+//       const addlist = document.createElement("button");
+//       addlist.setAttribute("class", "add-to-wishlist round-icon-btn");
+
+//       const iconlist = document.createElement("i");
+//       iconlist.setAttribute("class", "icon_heart_alt");
+
+//       const addcart = document.createElement("button");
+//       addcart.setAttribute("class", "add-to-cart round-icon-btn");
+
+//       const iconcart = document.createElement("i");
+//       iconcart.setAttribute("class", "icon_bag_alt");
+
+//       const addcomp = document.createElement("button");
+//       addcomp.setAttribute("class", "add-to-compare round-icon-btn");
+
+//       const iconcomp = document.createElement("i");
+//       iconcomp.setAttribute("class", "fas fa-random");
+
+//       const addview = document.createElement("button");
+//       addview.setAttribute("class", "quickview round-icon-btn");
+
+//       const iconview = document.createElement("i");
+//       iconview.setAttribute("class", "far fa-eye");
+
+//       subcontainer.appendChild(product);
+
+//       product.appendChild(img);
+//       product.appendChild(h5);
+
+//       product.appendChild(h3cost);
+
+//       product.appendChild(h3);
+
+//       product.appendChild(productselect);
+
+//       productselect.appendChild(addlist);
+
+//       addlist.appendChild(iconlist);
+
+//       productselect.appendChild(addcart);
+
+//       addcart.appendChild(iconcart);
+
+//       productselect.appendChild(addcomp);
+
+//       addcomp.appendChild(iconcomp);
+
+//       productselect.appendChild(addview);
+
+//       addview.appendChild(iconview);
+//     });
+//   });
