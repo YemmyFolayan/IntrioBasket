c9bab6dd523bfb58734042e9c7238634ea008c88
YemmyFolayan <foyemc@gmail.com>
2020-09-08 01:02:24 +0100

    integrating user registration API

--- assets/js/login.js
+++ assets/js/login.js
@@ -30,20 +30,102 @@ Form.addEventListener("submit", function (e) {
      
 
       if (msg == "Log in Successful") {
+        
+        const name = email;
+        const container = document.getElementById("containerr");
+        const loader = document.createElement("div");
+        loader.className = "progress";
+        const loadingBar = document.createElement("div");
+        loadingBar.className = "indeterminate";
+        loader.appendChild(loadingBar);
+        container.appendChild(loader);
+        setTimeout(function () {
+          const loaderDiv = document.querySelector("div.progress");
+          const panel = document.createElement("div");
+          panel.className = "card-panel green";
+          const text = document.createElement("span");
+          text.className = "white-text";
+          text.appendChild(document.createTextNode(`Log in Succesfully !, welcome to IntrioBasket ${name}`));
+          panel.appendChild(text);
+          container.replaceChild(panel, loaderDiv);
+        }, 1000);
+
         window.location.assign("/index.html");
         localStorage.setItem("login", true);
         console.log("logged in");
 
       } else if (msg == "Incorrect Email or Password") {
 
+        const name = email;
+        const container = document.getElementById("containerr");
+        const loader = document.createElement("div");
+        loader.className = "progress";
+        const loadingBar = document.createElement("div");
+        loadingBar.className = "indeterminate";
+        loader.appendChild(loadingBar);
+        container.appendChild(loader);
+        setTimeout(function () {
+          const loaderDiv = document.querySelector("div.progress");
+          const panel = document.createElement("div");
+          panel.className = "card-panel green";
+          const text = document.createElement("span");
+          text.className = "white-text";
+          text.appendChild(document.createTextNode(`Incorrect Email or Password`));
+          panel.appendChild(text);
+          container.replaceChild(panel, loaderDiv);
+        }, 1000);
+
+
         console.log("Incorrect Email or Password");
       } else if (msg == "Email not found") {
 
+        const name = email;
+        const container = document.getElementById("containerr");
+        const loader = document.createElement("div");
+        loader.className = "progress";
+        const loadingBar = document.createElement("div");
+        loadingBar.className = "indeterminate";
+        loader.appendChild(loadingBar);
+        container.appendChild(loader);
+        setTimeout(function () {
+          const loaderDiv = document.querySelector("div.progress");
+          const panel = document.createElement("div");
+          panel.className = "card-panel green";
+          const text = document.createElement("span");
+          text.className = "white-text";
+          text.appendChild(document.createTextNode(`Email not found`));
+          panel.appendChild(text);
+          container.replaceChild(panel, loaderDiv);
+        }, 1000);
+
+
+
         console.log("Email not found");
       } else {
+
+        const name = email;
+        const container = document.getElementById("containerr");
+        const loader = document.createElement("div");
+        loader.className = "progress";
+        const loadingBar = document.createElement("div");
+        loadingBar.className = "indeterminate";
+        loader.appendChild(loadingBar);
+        container.appendChild(loader);
+        setTimeout(function () {
+          const loaderDiv = document.querySelector("div.progress");
+          const panel = document.createElement("div");
+          panel.className = "card-panel green";
+          const text = document.createElement("span");
+          text.className = "white-text";
+          text.appendChild(
+            document.createTextNode(`An error occurred, Try Again!`)
+          );
+          panel.appendChild(text);
+          container.replaceChild(panel, loaderDiv);
+        }, 1000);
+
+        console.log("An error occurred, Try Again!");
         
-        console.log("not logged in");
-        console.log(JSON.stringify(msg) == "Log in Successful");
       }
     });
 });
