9b30b9c3f9c948678a9f3225ff32351462af6137
YemmyFolayan <foyemc@gmail.com>
2020-09-16 23:13:31 +0100

    added cartbutton badge to mobile views

--- js/app.js
+++ js/app.js
@@ -45,6 +45,8 @@ const addToCart = (id, name, type, imageUrl, price) => {
     updateCartButtonBadge()
 }
 
+
+
 /**
  * Update Cart Badge Function - Updates the Cart Button on the header section
  */
