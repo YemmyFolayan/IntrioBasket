07d7c2c619e7a6246cfb8e63ff07e96bad6b2e42
YemmyFolayan <foyemc@gmail.com>
2020-09-18 18:32:55 +0100

    added cartbutton badge to mobile views

--- js/services/shopDetails.js
+++ js/services/shopDetails.js
@@ -1,7 +1,7 @@
 const featuredProductDOM = document.getElementById("featuredProducts");
 
 const featuredProductItemTemplate = (productDetails) => {
-  return `
+    return `
         <div class="description-item_block">
             <div class="row align-items-center justify-content-around">
                 <div class="col-12 col-md-4">
@@ -14,36 +14,39 @@ const featuredProductItemTemplate = (productDetails) => {
                     </div>
                 </div>
             </div>
-        </div>`;
+        </div>`
+    
+    
 };
 
 // Returned first 9 products so as to make the design balance on the homepage
 const getFirst9 = (foodList) => {
-  return foodList.slice(15, 23);
+    return foodList.slice(0, 9)
 };
 
 const fetchFoodList = async () => {
-  const endpoint = "/food"; // THOUGHTS: There should be an endpoint for featured products...
+    const endpoint = '/food'; // THOUGHTS: There should be an endpoint for featured products...
 
-  const res = await api.request(endpoint); // TODO: handle errors..
+    const res = await api.request(endpoint); // TODO: handle errors..
 
-  const featuredProducts = getFirst9(res.payload);
-  featuredProducts.forEach((product) => {
-    let productDetails = {
-      id: product._id,
-      name: product.food_product_name,
-      type: product.product_type,
-      imageUrl: product.image_link,
-      price: product.cost,
-      description: product.long_description,
-      // TODO: there should also be a product url...
-    };
+    const featuredProducts = getFirst9(res.payload);
+    featuredProducts.forEach(product => {
 
-    let htmlString = featuredProductItemTemplate(productDetails);
-    let htmlFragment = document.createElement("div");
-    htmlFragment.innerHTML = htmlString;
-    featuredProductDOM.appendChild(htmlFragment);
-  });
+        let productDetails = {
+            id: product._id,
+            name: product.food_product_name,
+            type: product.product_type,
+            imageUrl: product.image_link,
+            price: product.cost,
+            description: product.long_description
+            // TODO: there should also be a product url...
+        };
+
+        let htmlString = featuredProductItemTemplate(productDetails);
+        let htmlFragment = document.createElement('div')
+        htmlFragment.innerHTML = htmlString
+        featuredProductDOM.appendChild(htmlFragment)
+    });
 };
 
 fetchFoodList();
