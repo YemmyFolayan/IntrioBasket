8eafac484ff854ae99a7429d06690704f44472a1
YemmyFolayan <foyemc@gmail.com>
2020-09-19 06:47:28 +0100

    added cartbutton badge to mobile views

--- js/services/paystackJS.js
+++ js/services/paystackJS.js
@@ -1,45 +1,40 @@
-//IMPLEMENTING PAY STACK TO THE CHECKOUT PAGE
+const paymentForm = document.getElementById('paymentForm');
 
+paymentForm.addEventListener("submit", payWithPaystack, false);
 
-var paymentForm = document.getElementById('paymentForm');
+function payWithPaystack(e) {
 
-paymentForm.addEventListener('submit', payWithPaystack, false);
+e.preventDefault();
 
-function payWithPaystack() {
+let handler = PaystackPop.setup({
 
-var handler = PaystackPop.setup({
+    key: 'pk_test_xxxxxxxxxx', // Replace with your public key
 
-    key: 'YOUR_PUBLIC_KEY', // Replace with your public key
+    email: document.getElementById("email-address").value,
 
-    email: document.getElementById('email-address').value,
+    amount: document.getElementById("amount").value * 100,
 
-    amount: document.getElementById('amount').value * 100, // the amount value is multiplied by 100 to convert to the lowest currency unit
+    firstname: document.getElementById("first-name").value,
 
-    currency: 'NGN', // Use GHS for Ghana Cedis or USD for US Dollars
+    lastname: document.getElementById("last-name").value,
 
-    firstname: document.getElementById('first-name').value,
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
 
-    lastname: document.getElementById('last-name').value,
+    // label: "Optional string that replaces customer email"
 
-    reference: 'YOUR_REFERENCE', // Replace with a reference you generated
+    onClose: function(){
 
-    callback: function(response) {
-
-    //this happens after the payment is completed successfully
-
-    var reference = response.reference;
-
-    alert('Payment complete! Reference: ' + reference);
-
-    // Make an AJAX call to your server with the reference to verify the transaction
+    alert('Window closed.');
 
     },
 
-    onClose: function() {
+    callback: function(response){
 
-    alert('Transaction was not completed, window closed.');
+    let message = 'Payment complete! Reference: ' + response.reference;
 
-    },
+    alert(message);
+
+    }
 
 });
 
