1e1e2018109eed49ecb920d9f5fa572849ec0bf4
Lekxel <sirlekxel@gmail.com>
2020-09-22 04:08:25 +0100

    feat: added cart item delete functionality

--- js/services/cart.js
+++ js/services/cart.js
@@ -8,28 +8,26 @@ const cartTotalTable = document.getElementById("cartTotalTable");
 const cartTotalTBodyDOM = document.getElementById("cartTotalTBody");
 
 const cartItemTemplate = (productDetails) => {
-  return `
+	return `
         <tr id="${productDetails.id}">
             <td class="product-name">${productDetails.name}</td>
             <td class="product-price">NGN ${productDetails.price}</td>
             <td class="product-quantity">
-            <input class="quantity no-round-input" type="number" min="1" value="${
-              productDetails.qty
-            }">
+            <input class="quantity no-round-input" type="number" min="1" value="${productDetails.qty
+		}">
             </td>
-            <td class="product-total">NGN ${
-              productDetails.price * productDetails.qty
-            }</td>
+            <td class="product-total">NGN ${productDetails.price * productDetails.qty
+		}</td>
             <td class="product-clear">
-            <button class="no-round-btn"><i class="icon_close"></i></button>
+            <button class="no-round-btn" onclick="deleteItem('${productDetails.id}')"><i class="icon_close"></i></button>
             </td>
         </tr>
     `;
 };
 
 
-const cartTotalTemplate = (totalPriceToPay,totalPriceToTax) => {
-  return `
+const cartTotalTemplate = (totalPriceToPay, totalPriceToTax) => {
+	return `
         <tr>
             <th>TOTAL:</th>
             <td>NGN ${totalPriceToPay}</td>
@@ -40,60 +38,60 @@ const cartTotalTemplate = (totalPriceToPay,totalPriceToTax) => {
 };
 
 const handleNoItemsInCart = () => {
-  updateCartButton.style.display = "none";
-  emptyCartButton.style.display = "none";
-  cartTotalTable.style.display = "none";
+	updateCartButton.style.display = "none";
+	emptyCartButton.style.display = "none";
+	cartTotalTable.style.display = "none";
 };
 
 const renderCartTotalTable = () => {
-  let totalPriceToPay = 0;
-  let totalPriceToTax = 0;
+	let totalPriceToPay = 0;
+	let totalPriceToTax = 0;
 
-  const cartStore = JSON.parse(localStorage.getItem(CONFIG.CART_STORE));
+	const cartStore = JSON.parse(localStorage.getItem(CONFIG.CART_STORE));
 
-  // Handle empty cart again!
-  if (cartStore === null || cartStore.length === 0)
-    return handleNoItemsInCart();
+	// Handle empty cart again!
+	if (cartStore === null || cartStore.length === 0)
+		return handleNoItemsInCart();
 
-  cartStore.forEach((product) => {
-    totalPriceToPay += product.price * product.qty;
-    totalPriceToTax =  totalPriceToPay + (totalPriceToPay * 0.075);
-    
-  });
+	cartStore.forEach((product) => {
+		totalPriceToPay += product.price * product.qty;
+		totalPriceToTax = totalPriceToPay + (totalPriceToPay * 0.075);
 
-  let htmlString = cartTotalTemplate(totalPriceToPay,totalPriceToTax);
+	});
 
-  cartTotalTBodyDOM.innerHTML = htmlString;
+	let htmlString = cartTotalTemplate(totalPriceToPay, totalPriceToTax);
+
+	cartTotalTBodyDOM.innerHTML = htmlString;
 };
 
 const lookUpCartStore = () => {
-  const cartStore = JSON.parse(localStorage.getItem(CONFIG.CART_STORE));
+	const cartStore = JSON.parse(localStorage.getItem(CONFIG.CART_STORE));
 
-  if (cartStore === null || cartStore.length === 0)
-    return handleNoItemsInCart();
+	if (cartStore === null || cartStore.length === 0)
+		return handleNoItemsInCart();
 
-  cartStore.forEach((product) => {
-    let htmlString = cartItemTemplate(product);
-    let htmlFragment = document.createElement("tr");
-    htmlFragment.setAttribute("id", product.id);
-    htmlFragment.innerHTML = htmlString;
-    shopCartTBodyDOM.appendChild(htmlFragment);
-  });
+	cartStore.forEach((product) => {
+		let htmlString = cartItemTemplate(product);
+		let htmlFragment = document.createElement("tr");
+		htmlFragment.setAttribute("id", product.id);
+		htmlFragment.innerHTML = htmlString;
+		shopCartTBodyDOM.appendChild(htmlFragment);
+	});
 
-  // call renderCartTotalTable
-  renderCartTotalTable();
+	// call renderCartTotalTable
+	renderCartTotalTable();
 
 
 };
 
 lookUpCartStore();
 
-  
+
 
 
 //productdetails template for shop_detail_fullwidth page
 const shopDetailTemplate = (productDetails) => {
-    return `
+	return `
             <div class="description-item_block">
                 <div class="row align-items-center justify-content-around">
                     <div class="col-12 col-md-4">
@@ -109,4 +107,26 @@ const shopDetailTemplate = (productDetails) => {
             </div>
             `;
 };
-    
+
+
+
+//delete each Item
+
+const deleteItem = id => {
+
+	if (localStorage.getItem(CONFIG.CART_STORE) === null) {
+		//something is wrong
+		return false
+	} else {
+		const cartList = JSON.parse(localStorage.getItem(CONFIG.CART_STORE));
+
+		let newCartList = cartList.filter((item, index) => item.id !== id)
+
+		localStorage.setItem(CONFIG.CART_STORE, JSON.stringify(newCartList));
+	}
+
+	// update Cart
+	shopCartTBodyDOM.innerHTML = ''
+	lookUpCartStore()
+}
+
