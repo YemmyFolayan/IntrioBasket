b3ece57d1d2c6c1f8b021fb88f1121a7ab47edf0
YemmyFolayan <foyemc@gmail.com>
2020-09-23 10:36:42 +0100

    added empty cart

--- js/services/shopDetails.js
+++ js/services/shopDetails.js
@@ -1,8 +1,7 @@
-
 const featuredProductDOM = document.getElementById("shopDetails");
 
 const featuredProductItemTemplate = (productDetails) => {
-    return `
+  return `
         <div class="description-item_block">
             <div class="row align-items-center justify-content-around">
                 <div class="col-12 col-md-4">
@@ -10,124 +9,45 @@ const featuredProductItemTemplate = (productDetails) => {
                 </div>
                 <div class="col-12 col-md-6">
                     <div class="description-item_text">
+                        <h2>${productDetails.type}</h2>
                         <h2>${productDetails.name}</h2>
+                        <h2>${productDetails.price}</h2>
                         <p>${productDetails.description}</p>
                     </div>
                 </div>
             </div>
-        </div>`
-    
-    
+        </div>`;
 };
-
 
 const fetchFoodList = async () => {
-    const endpoint = '/food'; // THOUGHTS: There should be an endpoint for featured products...
+  const endpoint = "/food"; // THOUGHTS: There should be an endpoint for featured products...
 
-    const res = await api.request(endpoint); // TODO: handle errors..
+  const res = await api.request(endpoint); // TODO: handle errors..
 
+  res.payload.forEach((product) => {
+    let productDetails = {
+      id: product._id,
+      name: product.food_product_name,
+      type: product.product_type,
+      imageUrl: product.image_link,
+      price: product.cost,
+      description: product.long_description,
+      // TODO: there should also be a product url...
+    };
 
-
-
-
-    res.payload.forEach((product) => {
-        let productDetails = {
-            id: product._id,
-            name: product.food_product_name,
-            type: product.product_type,
-            imageUrl: product.image_link,
-            price: product.cost,
-            description: product.long_description,
-            // TODO: there should also be a product url...
-        };
-
-
-        let htmlString = featuredProductItemTemplate(productDetails);
-        let htmlFragment = document.createElement('div');
-        htmlFragment.innerHTML = htmlString;
-        featuredProductDOM.appendChild(htmlFragment);
-    });
-
-    
+    let htmlString = featuredProductItemTemplate(productDetails);
+    let htmlFragment = document.createElement("div");
+    htmlFragment.innerHTML = htmlString;
+    featuredProductDOM.appendChild(htmlFragment);
+  });
 };
-    
+
 fetchFoodList();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // // Returned first 9 products so as to make the design balance on the homepage
 // const getFirst9 = (foodList) => {
 //     return foodList.slice(13, 23)
 // };
-
-
 
 // const fetchFoodList = async () => {
 //     const endpoint = '/food'; // THOUGHTS: There should be an endpoint for featured products...
