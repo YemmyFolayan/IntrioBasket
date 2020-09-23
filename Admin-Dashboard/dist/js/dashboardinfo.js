bf2f424eeb31d07e574e2032dfa312cddc0f4600
YemmyFolayan <foyemc@gmail.com>
2020-09-23 11:14:03 +0100

    added empty cart

---
+++ Admin-Dashboard/dist/js/dashboardinfo.js
@@ -0,0 +1,44 @@
+const featuredProductDOM = document.getElementById("featuredProducts");
+
+const featuredProductItemTemplate = (productDetails) => {
+  return `
+  
+  <div class="col-lg-3 col-6">
+  <!-- small box -->
+  <div class="small-box bg-warning">
+    <div class="inner">
+      <h3>44 ${productDetails.length}</h3>
+
+      <p>User Registrations</p>
+    </div>
+    <div class="icon">
+      <i class="ion ion-person-add"></i>
+    </div>
+    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+  </div>
+</div>
+    `;
+};
+
+
+const fetchFoodList = async () => {
+  const endpoint = "/food"; // THOUGHTS: There should be an endpoint for featured products...
+
+  const res = await api.request(endpoint); // TODO: handle errors..
+
+
+  res.payload.forEach((product) => {
+    let productDetails = {
+      length: product.length,
+      
+      // TODO: there should also be a product url...
+    };
+
+    let htmlString = featuredProductItemTemplate(productDetails);
+    let htmlFragment = document.createElement("div");
+    htmlFragment.innerHTML = htmlString;
+    featuredProductDOM.appendChild(htmlFragment);
+  });
+};
+
+fetchFoodList();
