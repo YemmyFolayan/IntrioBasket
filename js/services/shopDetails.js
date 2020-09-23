4480d14d4a27e983d828e01e8595ddb86329a73e
YemmyFolayan <foyemc@gmail.com>
2020-09-23 10:37:38 +0100

    added empty cart

--- js/services/shopDetails.js
+++ js/services/shopDetails.js
@@ -35,7 +35,7 @@ const fetchFoodList = async () => {
       // TODO: there should also be a product url...
     };
 
-    let htmlString = featuredProductItemTemplate(productDetails);
+    let htmlString = res.payload.forEach(featuredProductItemTemplate(productDetails));
     let htmlFragment = document.createElement("div");
     htmlFragment.innerHTML = htmlString;
     featuredProductDOM.appendChild(htmlFragment);
