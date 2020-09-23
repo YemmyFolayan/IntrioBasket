73444001a834dc864a95e9ce9adfaedf93bdbcce
YemmyFolayan <foyemc@gmail.com>
2020-09-24 00:44:53 +0100

    added empty cart

--- Admin-Dashboard/dist/js/dashboardinfo.js
+++ Admin-Dashboard/dist/js/dashboardinfo.js
@@ -18,47 +18,33 @@ const featuredProductItemTemplate = (count) => {
     `;
 };
 
-
-
 // GET
 
 // Get All Checkouts
 // http://intriobasket.pexceptos.com/api/checkout/get-all
-
 
 // GET
 
 // Get All Users
 // http://intriobasket.pexceptos.com/api/user/
 
-
-
 const fetchFoodList = async () => {
   const endpoint = "/user/"; // THOUGHTS: There should be an endpoint for featured products...
 
   const res = await api.request(endpoint); // TODO: handle errors..
 
-
   const count = res.payload.length;
-    
-    console.log(res.payload.length);
-    console.log("hi count");
 
-    let htmlString = featuredProductItemTemplate(count);
-    let htmlFragment = document.createElement("div");
-    htmlFragment.innerHTML = htmlString;
-    featuredProductDOM.appendChild(htmlFragment);
-  
+  console.log(res.payload.length);
+  console.log("hi count");
+
+  let htmlString = featuredProductItemTemplate(count);
+  let htmlFragment = document.createElement("div");
+  htmlFragment.innerHTML = htmlString;
+  featuredProductDOM.appendChild(htmlFragment);
 };
 
 fetchFoodList();
-
-
-
-
-
-
-
 
 const featuredProductsDOM = document.getElementById("allCheckout");
 
@@ -67,7 +53,7 @@ const featuredProductItemTemplates = (checks) => {
     <!-- small box -->
     <div class="small-box bg-danger">
       <div class="inner">
-        <h3>65 ${checks}</h3>
+        <h3>${checks}</h3>
 
         <p>All checkouts</p>
       </div>
@@ -80,41 +66,29 @@ const featuredProductItemTemplates = (checks) => {
     `;
 };
 
-
-
 // GET
 
 // Get All Checkouts
 // http://intriobasket.pexceptos.com/api/checkout/get-all
-
 
 // GET
 
 // Get All Users
 // http://intriobasket.pexceptos.com/api/user/
 
-
-
 const fetchFoodLists = async () => {
-  const endpoint = "/user/"; // THOUGHTS: There should be an endpoint for featured products...
-
+  const endpoint = "/user/"; // ADD CHECKOUTS ENDPOINT HERE // http://intriobasket.pexceptos.com/api/checkout/get-all
   const res = await api.request(endpoint); // TODO: handle errors..
 
-
   const checks = res.payload.length;
-    
-    console.log(res.payload.length);
-    console.log("hi count");
 
-    let htmlString = featuredProductItemTemplates(checks);
-    let htmlFragment = document.createElement("div");
-    htmlFragment.innerHTML = htmlString;
-    featuredProductsDOM.appendChild(htmlFragment);
-  
+  console.log(res.payload.length);
+  console.log("hi count");
+
+  let htmlString = featuredProductItemTemplates(checks);
+  let htmlFragment = document.createElement("div");
+  htmlFragment.innerHTML = htmlString;
+  featuredProductsDOM.appendChild(htmlFragment);
 };
 
 fetchFoodLists();
-
-
-
-
