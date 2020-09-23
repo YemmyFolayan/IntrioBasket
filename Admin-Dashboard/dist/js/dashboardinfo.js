daad187b1144a0bb3867295acf0c5fdffb1d2a04
YemmyFolayan <foyemc@gmail.com>
2020-09-24 00:43:03 +0100

    added empty cart

--- Admin-Dashboard/dist/js/dashboardinfo.js
+++ Admin-Dashboard/dist/js/dashboardinfo.js
@@ -52,3 +52,69 @@ const fetchFoodList = async () => {
 };
 
 fetchFoodList();
+
+
+
+
+
+
+
+
+const featuredProductsDOM = document.getElementById("allCheckout");
+
+const featuredProductItemTemplates = (checks) => {
+  return `
+    <!-- small box -->
+    <div class="small-box bg-danger">
+      <div class="inner">
+        <h3>65 ${checks}</h3>
+
+        <p>All checkouts</p>
+      </div>
+      <div class="icon">
+        <i class="ion ion-pie-graph"></i>
+      </div>
+      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+    </div>
+
+    `;
+};
+
+
+
+// GET
+
+// Get All Checkouts
+// http://intriobasket.pexceptos.com/api/checkout/get-all
+
+
+// GET
+
+// Get All Users
+// http://intriobasket.pexceptos.com/api/user/
+
+
+
+const fetchFoodLists = async () => {
+  const endpoint = "/user/"; // THOUGHTS: There should be an endpoint for featured products...
+
+  const res = await api.request(endpoint); // TODO: handle errors..
+
+
+  const checks = res.payload.length;
+    
+    console.log(res.payload.length);
+    console.log("hi count");
+
+    let htmlString = featuredProductItemTemplates(checks);
+    let htmlFragment = document.createElement("div");
+    htmlFragment.innerHTML = htmlString;
+    featuredProductsDOM.appendChild(htmlFragment);
+  
+};
+
+fetchFoodLists();
+
+
+
+
