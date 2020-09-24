cb309b2ad2710087890a833243e9ab332049d196
YemmyFolayan <foyemc@gmail.com>
2020-09-24 03:09:33 +0100

    dashboard : All checkouts & Registered user

--- js/services/shopDetails.js
+++ js/services/shopDetails.js
@@ -25,31 +25,22 @@ const featuredProductItemTemplate = (productDetails) => {
 //   return foodList.slice(0, 9);
 // };
 
-const fetchFoodList = async () => {
-  const endpoint = "/food"; // THOUGHTS: There should be an endpoint for featured products...
+const lookUpCartStore = () => {
+  const cartStore = JSON.parse(localStorage.getItem(CONFIG.CART_STORE));
 
-  const res = await api.request(endpoint); // TODO: handle errors..
-
-  res.payload.forEach((product) => {
-    let productDetails = {
-      id: product._id,
-      name: product.food_product_name,
-      type: product.product_type,
-      imageUrl: product.image_link,
-      price: product.cost,
-      description: product.long_description,
-      // TODO: there should also be a product url...
-    };
-
+  cartStore.forEach((product) => {
+  
     let htmlString = featuredProductItemTemplate(productDetails);
     let htmlFragment = document.createElement("div");
     htmlFragment.setAttribute("id", product.id);
     htmlFragment.innerHTML = htmlString;
     featuredProductDOM.appendChild(htmlFragment);
   });
-};
+}
 
-fetchFoodList();
+lookUpCartStore();
+
+
 
 // // Returned first 9 products so as to make the design balance on the homepage
 // const getFirst9 = (foodList) => {
