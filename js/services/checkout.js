830f2c059f91a2b8564cd949603a1461f9eb7ea8
YemmyFolayan <foyemc@gmail.com>
2020-09-27 13:42:51 +0100

    Shop details : including add to cart button

--- js/services/checkout.js
+++ js/services/checkout.js
@@ -15,21 +15,21 @@ const cartTotalTBodyDOM = document.getElementById("cartTotalTBody");
 
 //cart
 const cartItemTemplate = (productDetails) => {
-return `
+  return `
         <tr id="${productDetails.id}">
             <td class="product-name">${productDetails.name}</td>
             <td class="product-price">NGN ${productDetails.price}</td>
             <td class="product-quantity">
             <input class="quantity no-round-input" type="number" min="1" value="${
-            productDetails.qty
+              productDetails.qty
             }">
             </td>
             <td class="product-total">NGN ${
-            productDetails.price * productDetails.qty
+              productDetails.price * productDetails.qty
             }</td>
             <td class="product-clear">
             <button class="no-round-btn" onclick="deleteItem('${
-            productDetails.id
+              productDetails.id
             }')"><i class="icon_close"></i></button>
             </td>
         </tr>
@@ -37,7 +37,7 @@ return `
 };
 
 const cartTotalTemplate = (totalPriceToPay, totalPriceToTax) => {
-return `
+  return `
         <tr>
             <th>TOTAL:</th>
             <td>NGN ${totalPriceToPay}</td>
@@ -73,41 +73,41 @@ return `
 // };
 
 const handleNoItemsInCart = () => {
-updateCartButton.style.display = "none";
-emptyCartButton.style.display = "none";
-//cartTotalTable.style.display = "none";
+  updateCartButton.style.display = "none";
+  emptyCartButton.style.display = "none";
+  //cartTotalTable.style.display = "none";
 };
 
 const renderCartTotalTable = () => {
-let totalPriceToPay = 0;
-let totalPriceToTax = 0;
+  let totalPriceToPay = 0;
+  let totalPriceToTax = 0;
 
-const cartStore = JSON.parse(localStorage.getItem(CONFIG.CART_STORE));
+  const cartStore = JSON.parse(localStorage.getItem(CONFIG.CART_STORE));
 
-// Handle empty cart again!
-if (cartStore === null || cartStore.length === 0)
+  // Handle empty cart again!
+  if (cartStore === null || cartStore.length === 0)
     return handleNoItemsInCart();
 
-cartStore.forEach((product) => {
+  cartStore.forEach((product) => {
     totalPriceToPay += product.price * product.qty;
     totalPriceToTax = totalPriceToPay + totalPriceToPay * 0.075;
-});
+  });
 
-let htmlString = cartTotalTemplate(totalPriceToPay, totalPriceToTax);
-// //cart
-// let htmlStrings = cartTotalTemplateCheckout(totalPriceToPay, totalPriceToTax);
+  let htmlString = cartTotalTemplate(totalPriceToPay, totalPriceToTax);
+  // //cart
+  // let htmlStrings = cartTotalTemplateCheckout(totalPriceToPay, totalPriceToTax);
 
-cartTotalTBodyDOM.innerHTML = htmlString;
+  cartTotalTBodyDOM.innerHTML = htmlString;
 };
 
 const lookUpCartStore = () => {
-const cartStore = JSON.parse(localStorage.getItem(CONFIG.CART_STORE));
+  const cartStore = JSON.parse(localStorage.getItem(CONFIG.CART_STORE));
 
-if (cartStore === null || cartStore.length === 0)
+  if (cartStore === null || cartStore.length === 0)
     //// Create User Cart here from back end http://intriobasket.pexceptos.com/api/user/create-cart/5f4d0fd68cc9aa11e6151b88
     return handleNoItemsInCart();
 
-cartStore.forEach((product) => {
+  cartStore.forEach((product) => {
     let htmlString = cartItemTemplate(product);
     //checkout
 
@@ -127,10 +127,10 @@ cartStore.forEach((product) => {
     // htmlFragments.innerHTML = htmlStrings;
 
     // shopCartTBodyDOMCheckout.appendChild(htmlFragments);
-});
+  });
 
-// call renderCartTotalTable
-renderCartTotalTable();
+  // call renderCartTotalTable
+  renderCartTotalTable();
 };
 
 lookUpCartStore();
@@ -138,21 +138,21 @@ lookUpCartStore();
 //delete each Item
 
 const deleteItem = (id) => {
-if (localStorage.getItem(CONFIG.CART_STORE) === null) {
+  if (localStorage.getItem(CONFIG.CART_STORE) === null) {
     //something is wrong
     return false;
-} else {
+  } else {
     const cartList = JSON.parse(localStorage.getItem(CONFIG.CART_STORE));
 
     let newCartList = cartList.filter((item, index) => item.id !== id);
 
     localStorage.setItem(CONFIG.CART_STORE, JSON.stringify(newCartList));
-}
+  }
 
-// update Cart
-shopCartTBodyDOM.innerHTML = "";
+  // update Cart
+  shopCartTBodyDOM.innerHTML = "";
 
-lookUpCartStore();
+  lookUpCartStore();
 };
 
 //TODO
