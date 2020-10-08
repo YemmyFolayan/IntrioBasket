3efd5a1f5ad85c2df178e68c48a91efba7fb0883
YemmyFolayan <foyemc@gmail.com>
2020-10-08 22:18:11 +0100

    cart to database

--- js/app.js
+++ js/app.js
@@ -35,7 +35,7 @@ const url = `http://intriobasket.pexceptos.com/api/user/create-cart/${userId}`;
 
 console.log(url);
 
-const addToCart = (id, name, type, imageUrl, price) => {
+const addToCart = (id, name, type, imageUrl, price, qty) => {
   const productDetails = { id, name, type, imageUrl, price, qty: 1 };
 
   if (localStorage.getItem(CONFIG.CART_STORE) === null) {
