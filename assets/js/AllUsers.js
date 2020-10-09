558e8c223cda1e7428b548e4cd8e33dd69a7429e
YemmyFolayan <foyemc@gmail.com>
2020-10-09 15:38:18 +0100

    cart to database

--- assets/js/AllUsers.js
+++ assets/js/AllUsers.js
@@ -4,16 +4,10 @@ const featuredProductItemTemplate = (userDetails) => {
   return `
   <div class="product">
   <h3 class="product-price">${userDetails.fullname}</h3>
-  <h3 class="product-type">${userDetails.email}</h3>
+  <h3 class="product-price">${userDetails.email}</h3>
   <h3 class="product-price">${userDetails.phonenumber}</h3>
-  <h3 class="product-type">${userDetails.gender}</h3>
+  <h3 class="product-price">${userDetails.gender}</h3>
   <div class="product-select">
-      <button class="add-to-wishlist round-icon-btn">
-          <i class="icon_heart_alt"></i>
-      </button>
-      <button class="add-to-cart round-icon-btn">
-          <i class="fa fa-shopping-cart"></i>
-      </button>
       <button class="quickview round-icon-btn">
           <i class="far fa-eye"></i>
       </button>
