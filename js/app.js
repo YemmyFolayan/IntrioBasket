db27c7f49c264e1e57e5835cd3de94ef774b2af2
YemmyFolayan <foyemc@gmail.com>
2020-10-13 09:27:01 +0100

    checkout

--- js/app.js
+++ js/app.js
@@ -32,6 +32,56 @@ const updateCartButtonBadge = () => {
   });
 };
 
+
+console.log("CART DATA");
+const GetUserCart = () => {
+  console.log("updateCheckout function");
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("x-access-token", `${userToken}`);
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch(
+    `https://cors-anywhere.herokuapp.com/http://intriobasket.pexceptos.com/api/user/${userId}`,
+    requestOptions
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const res = data;
+      console.log("CART FETCH");
+      console.log(res.payload.cart);
+      const cartDetails = res.payload.cart;
+
+      res.payload.cart.forEach((cart) => {
+        let cartDetails = {
+          name: cart.item_name,
+          qty: cart.number,
+          price: cart.initial_cost,
+          imageUrl: cart.item_image,
+        };
+        console.log(cartDetails);
+        console.log("carttyyyyyyy");
+
+        //I'll use cartDetails here
+        localStorage.setItem("cartDetails", cartDetails);
+      });
+    })
+
+    .catch((error) => console.log("error", error));
+
+  console.log("GetUserCart");
+};
+
+GetUserCart();
+
 // Mini Router (refreshes the page)
 //ROUTER for page redirect
 const router = (url) => {
@@ -633,54 +683,6 @@ DisplayUserNameTemplate();
 
 
 
-console.log("CART DATA");
-const GetUserCart = () => {
-  console.log("updateCheckout function");
-
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("x-access-token", `${userToken}`);
-
-  var requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
-  };
-
-  fetch(
-    `https://cors-anywhere.herokuapp.com/http://intriobasket.pexceptos.com/api/user/${userId}`,
-    requestOptions
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      const res = data;
-      console.log("CART FETCH");
-      console.log(res.payload.cart);
-      const cartDetails = res.payload.cart;
-
-      res.payload.cart.forEach((cart) => {
-        let cartDetails = {
-          name: cart.item_name,
-          qty: cart.number,
-          price: cart.initial_cost,
-          imageUrl: cart.item_image,
-        };
-        console.log(cartDetails);
-        console.log("carttyyyyyyy");
-
-        //I'll use cartDetails here
-        localStorage.setItem("cartDetails", cartDetails);
-      });
-    })
-
-    .catch((error) => console.log("error", error));
-
-  console.log("GetUserCart");
-};
-
-GetUserCart();
 
 /**username Mobile
 const CLIENTNameMobileDOM = document.getElementById("userNameMobile");
