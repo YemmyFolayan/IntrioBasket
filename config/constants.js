6e1a80416ae90e3f00dde4e2aab24ea4b450b589
YemmyFolayan <foyemc@gmail.com>
2020-10-15 18:55:43 +0100

    checkout

--- config/constants.js
+++ config/constants.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    API_BASE: 'http://intriobasket.pexceptos.com/api', // TODO: change this URL on production
-    CART_STORE: 'cart_store'
+  API_BASE: "http://intriobasket.pexceptos.com/api", // TODO: change this URL on production
+  CART_STORE: "cart_store",
 };
